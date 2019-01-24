a99600f8bbf20c25db390ad19508d0aa40262709
susieo <susytuty@gmail.com>
2019-01-23 20:29:04 -0800

    database

---
+++ backend/User.js
@@ -0,0 +1,15 @@
+// /backend/data.js
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// this will be our data base's data structure 
+const UserSchema = new Schema(
+  {
+    name: String,
+    lastname: String,
+    email: String
+  }
+);
+
+// export the new Schema so we could modify it using Node.js
+module.exports = mongoose.model("Users", UserSchema);
